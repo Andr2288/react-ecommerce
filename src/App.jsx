dd0c4a7a14c32dbea6479fcc965ca9bffd1a3376
chorn <andriychornookyi228@gmail.com>
2025-11-15 16:24:58 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {Routes, Route, Navigate} from "react-router-dom";
+
+import Navbar from "./components/Navbar.jsx";
+
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+
+import {useAuthStore} from "./store/useAuthStore.js";
+import {useEffect} from "react";
+
+import {Loader} from "lucide-react";
+
+const App = () => {
+
+    const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
+
+    console.log({authUser});
+
+    if (isCheckingAuth && !authUser) {
+        return (
+            <div className="flex items-center justify-center h-screen">
+                <Loader className="size-10 animate-spin" />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+
+            <Navbar />
+
+            <Routes>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default App
